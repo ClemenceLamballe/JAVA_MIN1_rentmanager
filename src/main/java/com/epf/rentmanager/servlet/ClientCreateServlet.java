138ed5c42042f96d98d47bb4fd3359dc0f49d15e
CLamballe <clemence.lamballe@epfedu.fr>
2024-03-04 18:12:35 +0100

    creation de client

---
+++ src/main/java/com/epf/rentmanager/servlet/ClientCreateServlet.java
@@ -0,0 +1,57 @@
+package com.epf.rentmanager.servlet;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.epf.rentmanager.dao.DaoException;
+import com.epf.rentmanager.model.Client;
+import com.epf.rentmanager.service.ClientService;
+import com.epf.rentmanager.service.ServiceException;
+
+@WebServlet("/users/create")
+public class ClientCreateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Display the create client form
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/users/create.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            // Retrieve client details from the form
+            String firstName = request.getParameter("first_name");
+
+            String lastName = request.getParameter("last_name");
+
+            String email = request.getParameter("email");
+            LocalDate birthdate = LocalDate.parse(request.getParameter("birthdate"));
+
+            // Create a new Client object
+            Client newClient = new Client(-1, lastName, firstName, email, birthdate);
+
+            // Call the create method in the service layer
+            ClientService clientService = ClientService.getInstance();
+            long generatedId = clientService.create(newClient);
+
+            // Update the ID after creation
+            newClient.setId(generatedId);
+
+            // Redirect to the client list page
+            response.sendRedirect(request.getContextPath() + "/clients/list");
+        } catch (ServiceException | DaoException e) {
+            // Handle exceptions (e.g., redirect to an error page)
+            e.printStackTrace(); // For now, print the stack trace.
+        }
+    }
+}
--- src/main/webapp/WEB-INF/views/users/list.jsp
+++ src/main/webapp/WEB-INF/views/users/list.jsp
@@ -31,6 +31,7 @@
                                     <th>Nom</th>
                                     <th>Prenom</th>
                                     <th>Email</th>
+                                    <th>Naissance</th>
                                     <th>Action</th>
                                 </tr>
 
@@ -41,6 +42,7 @@
                                         <td>${client.nom}</td>
                                         <td>${client.prenom}</td>
                                         <td>${client.email}</td>
+                                        <td>${client.naissance}</td>
                                         <td>
                                             <a class="btn btn-primary" href="${pageContext.request.contextPath}/users/details?id=${client.id}">
                                                 <i class="fa fa-play"></i>
